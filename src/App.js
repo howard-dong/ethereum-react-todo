a45002d13452235b00b15ffeaff7a80198e9336e
nex135 <natnevik@gmail.com>
2021-03-24 16:02:41 -0400

    mild setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
-import Todolist from ' ./TodoList.js'
+import "./App.css";
+import React, {useState, useEffect} from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+
 function App() {
   // return (
   //   <div className="App">
@@ -20,31 +23,45 @@ function App() {
   //     </header>
   //   </div>
   // );
+  const [todos, setTodos] = useState([]);
+  function addTodo(todo){
+    setTodos([todo, ...todos]);
+  }
 
   return (
-    <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap py-3 shadow">
-        <a className="navbar-brand mx-auto">ETHEREUM TODOLIST</a>
-        <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <small><a className="nav-link" href="#"><span id="account"></span></a></small>
-          </li>
-        </ul>
-      </nav>
-      <div className="container-fluid">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex justify-content-center">
-            {/* { this.state.loading
-              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-              : <TodoList
-                tasks={this.state.tasks}
-                createTask={this.createTask}
-                toggleCompleted={this.toggleCompleted} />
-            } */}
-          </main>
-        </div>
-      </div>
+    <div className = "App">
+      <header className = "App-Header">
+        <p>React Todo</p>
+        <TodoForm addTodo = {addTodo} />
+        <TodoList todos = {todos} />
+      </header>
     </div>
+
+
+
+    // <div>
+    //   <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap py-3 shadow">
+    //     <a className="navbar-brand mx-auto">ETHEREUM TODOLIST</a>
+    //     <ul className="navbar-nav px-3">
+    //       <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+    //         <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+    //       </li>
+    //     </ul>
+    //   </nav>
+    //   <div className="container-fluid">
+    //     <div className="row">
+    //       <main role="main" className="col-lg-12 d-flex justify-content-center">
+    //         {/* { this.state.loading
+    //           ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+    //           : <TodoList
+    //             tasks={this.state.tasks}
+    //             createTask={this.createTask}
+    //             toggleCompleted={this.toggleCompleted} />
+    //         } */}
+    //       </main>
+    //     </div>
+    //   </div>
+    // </div>
   );
     
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,50 +1,61 @@
-import React, { Component } from 'react'
+import React from "react";
+import Todo from "./Todo";
 
-class TodoList extends Component {
+// class TodoList extends Component {
 
-  render() {
+//   render() {
+//     return (
+//       <div id="content">
+//         <form onSubmit={(event) => {
+//           event.preventDefault()
+//           this.props.createTask(this.task.value)
+//         }}>
+//           <input
+//             id="newTask"
+//             ref={(input) => {
+//               this.task = input
+//             }}
+//             type="text"
+//             className="form-control"
+//             placeholder="Add task..."
+//             required />
+//           <input type="submit" hidden={true} />
+//         </form>
+//         <ul id="taskList" className="list-unstyled">
+//           { this.props.tasks.map((task, key) => {
+//             return(
+//               <div className="taskTemplate" className="checkbox" key={key}>
+//                 <label>
+//                   <input
+//                   type="checkbox"
+//                   name={task.id}
+//                   defaultChecked={task.completed}
+//                   ref={(input) => {
+//                     this.checkbox = input
+//                   }}
+//                   onClick={(event) => {
+//                     this.props.toggleCompleted(this.checkbox.name) }}/>
+//                   <span className="content">{task.content}</span>
+//                 </label>
+//               </div>
+//             )
+//           })}
+//         </ul>
+//         <ul id="completedTaskList" className="list-unstyled">
+//         </ul>
+//       </div>
+//     );
+//   }
+// }
+
+function TodoList({ todos}){
     return (
-      <div id="content">
-        <form onSubmit={(event) => {
-          event.preventDefault()
-          this.props.createTask(this.task.value)
-        }}>
-          <input
-            id="newTask"
-            ref={(input) => {
-              this.task = input
-            }}
-            type="text"
-            className="form-control"
-            placeholder="Add task..."
-            required />
-          <input type="submit" hidden={true} />
-        </form>
-        <ul id="taskList" className="list-unstyled">
-          { this.props.tasks.map((task, key) => {
-            return(
-              <div className="taskTemplate" className="checkbox" key={key}>
-                <label>
-                  <input
-                  type="checkbox"
-                  name={task.id}
-                  defaultChecked={task.completed}
-                  ref={(input) => {
-                    this.checkbox = input
-                  }}
-                  onClick={(event) => {
-                    this.props.toggleCompleted(this.checkbox.name) }}/>
-                  <span className="content">{task.content}</span>
-                </label>
-              </div>
-            )
-          })}
+        <ul>
+            {todos.map(todo =>(
+                <Todo key={todo.id} todo={todo} />
+            ))}
         </ul>
-        <ul id="completedTaskList" className="list-unstyled">
-        </ul>
-      </div>
-    );
-  }
+    )
 }
 
 export default TodoList;
