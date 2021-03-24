009cd7ec6b45fe0e3e212234c855a2f1599b9d8f
Rachel Li <li.rachel.cheng@gmail.com>
2021-03-24 13:07:58 -0400

    basic css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex justify-content-center">
+            {/* { this.state.loading
+              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+              : <TodoList
+                tasks={this.state.tasks}
+                createTask={this.createTask}
+                toggleCompleted={this.toggleCompleted} />
+            } */}
+          </main>
+        </div>
+      </div>
     </div>
   );
-}
+    
+  }
 
 export default App;
