442fc7c14e9e3b7b25d6a0f1c836a1dd1d995ff0
a2zain <a2zain@uwaterloo.ca>
2021-03-25 15:58:51 -0400

    front end attempt

--- src/App.js
+++ src/App.js
@@ -1,69 +1,81 @@
-import logo from './logo.svg';
-import "./App.css";
-import React, {useState, useEffect} from "react";
-import TodoForm from "./TodoForm";
-import TodoList from "./TodoList";
+import React, { Component } from 'react'
+import Web3 from 'web3'
+import './App.css'
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './config'
+import TodoList from './TodoList'
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-  const [todos, setTodos] = useState([]);
-  function addTodo(todo){
-    setTodos([todo, ...todos]);
+function App(props) {
+  const componentWillMount = () => {
+    loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:8545")
+    const accounts = await web3.eth.getAccounts()
+    setState({ account: accounts[0] })
+    const todoList = new web3.eth.Contract(TODO_LIST_ABI, TODO_LIST_ADDRESS)
+    setState({ todoList })
+    const taskCount = await todoList.methods.taskCount().call()
+    setState({ taskCount })
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await todoList.methods.tasks(i).call()
+      setState({
+        tasks: [...state.tasks, task]
+      })
+    }
+    setState({ loading: false })
+  }
+
+  function Apps(props) {
+      const [account, setAccount] = useState('')
+      const [taskCount, setTaskCount] = useState(0)
+      const [tasks, setTasks] = useState([])
+      const [loading, setLoading] = useState(true)
+    }
+
+  }
+
+  const createTask = (content) => {
+    setState({ loading: true })
+    state.todoList.methods.createTask(content).send({ from: state.account })
+    .once('receipt', (receipt) => {
+      setState({ loading: false })
+    })
+  }
+
+  toggleCompleted(taskId) {
+    setState({ loading: true })
+    state.todoList.methods.toggleCompleted(taskId).send({ from: state.account })
+    .once('receipt', (receipt) => {
+      setState({ loading: false })
+    })
   }
 
   return (
-    <div className = "App">
-      <header className = "App-Header">
-        <p>React Todo</p>
-        <TodoForm addTodo = {addTodo} />
-        <TodoList todos = {todos} />
-      </header>
+    <div>
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="http://www.dappuniversity.com/free-download" target="_blank">Dapp University | Todo List</a>
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+            <small><a className="nav-link" href="#"><span id="account"></span></a></small>
+          </li>
+        </ul>
+      </nav>
+      <div className="container-fluid">
+        <div className="row">
+          <main role="main" className="col-lg-12 d-flex justify-content-center">
+            { state.loading
+              ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
+              : <TodoList
+                tasks={state.tasks}
+                createTask={createTask}
+                toggleCompleted={toggleCompleted} />
+            }
+          </main>
+        </div>
+      </div>
     </div>
-
-
-
-    // <div>
-    //   <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap py-3 shadow">
-    //     <a className="navbar-brand mx-auto">ETHEREUM TODOLIST</a>
-    //     <ul className="navbar-nav px-3">
-    //       <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-    //         <small><a className="nav-link" href="#"><span id="account"></span></a></small>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    //   <div className="container-fluid">
-    //     <div className="row">
-    //       <main role="main" className="col-lg-12 d-flex justify-content-center">
-    //         {/* { this.state.loading
-    //           ? <div id="loader" className="text-center"><p className="text-center">Loading...</p></div>
-    //           : <TodoList
-    //             tasks={this.state.tasks}
-    //             createTask={this.createTask}
-    //             toggleCompleted={this.toggleCompleted} />
-    //         } */}
-    //       </main>
-    //     </div>
-    //   </div>
-    // </div>
   );
-    
-  }
+}
 
 export default App;
